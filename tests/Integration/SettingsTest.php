12b88ab451365cadba1af979cce864f58d0ac072
Bruno Casali <brunoocasali@gmail.com>
2022-03-16 14:42:52 -0300

    Remove `testGetDefaultSettings` since this test should be in the meilisearch-php

--- tests/Integration/SettingsTest.php
+++ tests/Integration/SettingsTest.php
@@ -40,41 +40,6 @@ class SettingsTest extends BaseKernelTestCase
         $this->application = new Application(self::$kernel);
     }
 
-    public function testGetDefaultSettings(): void
-    {
-        $primaryKey = 'ObjectID';
-        $indexAName = $this->getPrefix().'indexA';
-        $indexBName = $this->getPrefix().'indexB';
-        $this->client->createIndex($indexAName);
-        $this->client->createIndex($indexBName, ['primaryKey' => $primaryKey]);
-
-        $firstTask = $this->client->getTasks()['results'][0];
-        $this->client->waitForTask($firstTask['uid']);
-
-        $settingA = $this->client->index($indexAName)->getSettings();
-        $settingB = $this->client->index($indexBName)->getSettings();
-
-        $this->assertEquals(self::DEFAULT_RANKING_RULES, $settingA['rankingRules']);
-        $this->assertNull($settingA['distinctAttribute']);
-        $this->assertIsArray($settingA['searchableAttributes']);
-        $this->assertEquals(['*'], $settingA['searchableAttributes']);
-        $this->assertIsArray($settingA['displayedAttributes']);
-        $this->assertEquals(['*'], $settingA['displayedAttributes']);
-        $this->assertIsArray($settingA['stopWords']);
-        $this->assertEmpty($settingA['stopWords']);
-        $this->assertIsArray($settingA['synonyms']);
-        $this->assertEmpty($settingA['synonyms']);
-
-        $this->assertEquals(self::DEFAULT_RANKING_RULES, $settingB['rankingRules']);
-        $this->assertNull($settingB['distinctAttribute']);
-        $this->assertEquals(['*'], $settingB['searchableAttributes']);
-        $this->assertEquals(['*'], $settingB['displayedAttributes']);
-        $this->assertIsArray($settingB['stopWords']);
-        $this->assertEmpty($settingB['stopWords']);
-        $this->assertIsArray($settingB['synonyms']);
-        $this->assertEmpty($settingB['synonyms']);
-    }
-
     public function testUpdateSettings(): void
     {
         $index = $this->getPrefix().self::$indexName;
