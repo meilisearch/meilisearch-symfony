2c9162a0c29ae50bd85c1a0adcd4360ab11bb1b5
Bruno Casali <brunoocasali@gmail.com>
2022-08-16 14:15:30 -0300

    Update version for the next release (v0.8.1)

--- src/MeiliSearchBundle.php
+++ src/MeiliSearchBundle.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 final class MeiliSearchBundle extends Bundle
 {
-    public const VERSION = '0.8.0';
+    public const VERSION = '0.8.1';
 
     public static function qualifiedVersion()
     {
