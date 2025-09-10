e479bc22ef247a8eff92fcd964ee007ed1702b36
Tomas Norkūnas <norkunas.tom@gmail.com>
2025-09-10 12:40:47 +0300

    Prepare release

--- src/MeilisearchBundle.php
+++ src/MeilisearchBundle.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class MeilisearchBundle extends Bundle
 {
-    public const VERSION = '0.15.8';
+    public const VERSION = '0.15.9';
 
     public static function qualifiedVersion(): string
     {
