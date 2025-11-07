39d97d57c9100defca8501d2d2a7bdcbecfc200e
Christopher Georg <christopher.georg@sr-travel.de>
2025-11-07 10:39:01 +0100

    Feature/switch to php based configs

--- config/services.php
+++ config/services.php
@@ -2,16 +2,29 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Meilisearch\Bundle\Command\MeilisearchClearCommand;
+use Meilisearch\Bundle\Command\MeilisearchCreateCommand;
+use Meilisearch\Bundle\Command\MeilisearchDeleteCommand;
+use Meilisearch\Bundle\Command\MeilisearchImportCommand;
+use Meilisearch\Bundle\Command\MeilisearchUpdateSettingsCommand;
+use Meilisearch\Bundle\Engine;
+use Meilisearch\Bundle\EventListener\DoctrineEventSubscriber;
+use Meilisearch\Bundle\SearchService;
+use Meilisearch\Bundle\Services\MeilisearchService;
+use Meilisearch\Bundle\Services\SettingsUpdater;
+use Meilisearch\Bundle\Services\UnixTimestampNormalizer;
+use Meilisearch\Client;
+
 return static function(ContainerConfigurator $container) {
     $services = $container->services();
     $parameters = $container->parameters();
 
-    $services->set('meilisearch.engine', \Meilisearch\Bundle\Engine::class)
+    $services->set('meilisearch.engine', Engine::class)
         ->args([service('meilisearch.client')]);
 
-    $services->alias(\Meilisearch\Bundle\Engine::class, 'meilisearch.engine');
+    $services->alias(Engine::class, 'meilisearch.engine');
 
-    $services->set('meilisearch.service', \Meilisearch\Bundle\Services\MeilisearchService::class)
+    $services->set('meilisearch.service', MeilisearchService::class)
         ->public()
         ->args([
             abstract_arg('normalizer'),
@@ -24,14 +37,14 @@ return static function(ContainerConfigurator $container) {
         ->public()
         ->deprecate('meilisearch/search-bundle', '0.14', 'The "%alias_id%" service alias is deprecated. Use "meilisearch.service" instead.');
 
-    $services->set('meilisearch.search_indexer_subscriber', \Meilisearch\Bundle\EventListener\DoctrineEventSubscriber::class)
+    $services->set('meilisearch.search_indexer_subscriber', DoctrineEventSubscriber::class)
         ->public()
         ->args([service('meilisearch.service')]);
 
     $services->alias('search.search_indexer_subscriber', 'meilisearch.search_indexer_subscriber')
         ->deprecate('meilisearch/search-bundle', '0.14', 'The "%alias_id%" service alias is deprecated. Use "meilisearch.search_indexer_subscriber" instead.');
 
-    $services->set('meilisearch.client', \Meilisearch\Client::class)
+    $services->set('meilisearch.client', Client::class)
         ->public()
         ->lazy()
         ->args([
@@ -47,25 +60,25 @@ return static function(ContainerConfigurator $container) {
         ->public()
         ->deprecate('meilisearch/search-bundle', '0.14', 'The "%alias_id%" service alias is deprecated. Use "meilisearch.client" instead.');
 
-    $services->alias(\Meilisearch\Client::class, 'meilisearch.client')
+    $services->alias(Client::class, 'meilisearch.client')
         ->public();
 
-    $services->alias(\Meilisearch\Bundle\SearchService::class, 'meilisearch.service');
+    $services->alias(SearchService::class, 'meilisearch.service');
 
-    $services->set('meilisearch.settings_updater', \Meilisearch\Bundle\Services\SettingsUpdater::class)
+    $services->set('meilisearch.settings_updater', SettingsUpdater::class)
         ->args([
             service('meilisearch.service'),
             service('meilisearch.client'),
             service('event_dispatcher'),
         ]);
 
-    $services->alias(\Meilisearch\Bundle\Services\SettingsUpdater::class, 'meilisearch.settings_updater');
+    $services->alias(SettingsUpdater::class, 'meilisearch.settings_updater');
 
-    $services->set(\Meilisearch\Bundle\Command\MeilisearchClearCommand::class)
+    $services->set(MeilisearchClearCommand::class)
         ->args([service('meilisearch.service')])
         ->tag('console.command', ['command' => 'meilisearch:clear|meili:clear', 'description' => 'Clear the index documents']);
 
-    $services->set(\Meilisearch\Bundle\Command\MeilisearchCreateCommand::class)
+    $services->set(MeilisearchCreateCommand::class)
         ->args([
             service('meilisearch.service'),
             service('meilisearch.client'),
@@ -74,11 +87,11 @@ return static function(ContainerConfigurator $container) {
         ])
         ->tag('console.command', ['command' => 'meilisearch:create|meili:create', 'description' => 'Create indexes']);
 
-    $services->set(\Meilisearch\Bundle\Command\MeilisearchDeleteCommand::class)
+    $services->set(MeilisearchDeleteCommand::class)
         ->args([service('meilisearch.service')])
         ->tag('console.command', ['command' => 'meilisearch:delete|meili:delete', 'description' => 'Delete the indexes']);
 
-    $services->set(\Meilisearch\Bundle\Command\MeilisearchImportCommand::class)
+    $services->set(MeilisearchImportCommand::class)
         ->args([
             service('meilisearch.service'),
             service('doctrine'),
@@ -88,7 +101,7 @@ return static function(ContainerConfigurator $container) {
         ])
         ->tag('console.command', ['command' => 'meilisearch:import|meili:import', 'description' => 'Import given entity into search engine']);
 
-    $services->set(\Meilisearch\Bundle\Command\MeilisearchUpdateSettingsCommand::class)
+    $services->set(MeilisearchUpdateSettingsCommand::class)
         ->args([
             service('meilisearch.service'),
             service('meilisearch.settings_updater'),
@@ -96,6 +109,6 @@ return static function(ContainerConfigurator $container) {
         ])
         ->tag('console.command', ['command' => 'meilisearch:update-settings', 'description' => 'Push settings to meilisearch']);
 
-    $services->set(\Meilisearch\Bundle\Services\UnixTimestampNormalizer::class)
+    $services->set(UnixTimestampNormalizer::class)
         ->tag('serializer.normalizer');
 };
